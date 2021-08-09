3141bc44af2a88b6aab0a56667adacf4f92752bb
Ayush Rawat <ayushrawat11562@gmail.com>
2021-08-09 14:15:20 +0530

    basic html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [screen, setscreen] = useState("press any button");
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="screen">
+          <h1>{screen}</h1>
+        </div>
+        <div className="btns">
+          <div className="first">
+            <button className="one">A</button>
+            <button className="two">S</button>
+            <button className="three">D</button>
+          </div>
+          <div className="sec">
+            <button className="four">F</button>
+            <button className="five">G</button>
+            <button className="six">H</button>
+          </div>
+          <div className="third">
+            <button className="seven">J</button>
+            <button className="eight">K</button>
+            <button className="nine">L</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
